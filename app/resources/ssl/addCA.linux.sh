06a3370c426d0083a0e48230dc7fee585c97e1b5
Павел Гильмутдинов <gilmutdinov@trusted.ru>
2020-08-18 15:41:11 +0300

    649-cryptoarm #https://git.digtlab.ru/trusted.ru/crypto-web/issues/45 add command for linux

--- app/resources/ssl/addCA.linux.sh
+++ app/resources/ssl/addCA.linux.sh
@@ -3,3 +3,4 @@
 #mkdir "/etc/opt/Trusted"
 mkdir -p "/etc/opt/Trusted/CryptoARM GOST"
 chmod 777 "/etc/opt/Trusted/CryptoARM GOST"
+xdg-mime default cryptoarm-gost.desktop x-scheme-handler/cryptoarm
