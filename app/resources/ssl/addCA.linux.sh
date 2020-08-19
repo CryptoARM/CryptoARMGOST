120c69b3d3e8e845400c53de8cd47abb625fb791
Павел Гильмутдинов <gilmutdinov@trusted.ru>
2020-08-19 10:28:54 +0300

    649-cryptoarm #https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/649 add sh script for register URI on linux

--- app/resources/ssl/addCA.linux.sh
+++ app/resources/ssl/addCA.linux.sh
@@ -3,4 +3,31 @@
 #mkdir "/etc/opt/Trusted"
 mkdir -p "/etc/opt/Trusted/CryptoARM GOST"
 chmod 777 "/etc/opt/Trusted/CryptoARM GOST"
-xdg-mime default cryptoarm-gost.desktop x-scheme-handler/cryptoarm
+# xdg-mime default cryptoarm-gost.desktop x-scheme-handler/cryptoarm
+# xdg-mime default CryptoARM_GOST.desktop x-scheme-handler/cryptoarm
+
+if dpkg -S /bin/ls >/dev/null 2>&1
+then
+  case "$(arch)" in
+    x86_64)
+      xdg-mime default CryptoARM_GOST.desktop x-scheme-handler/cryptoarm ;;
+    i368)
+      xdg-mime default CryptoARM_GOST.desktop x-scheme-handler/cryptoarm;;
+    *)
+      exit 1
+      ;;
+  esac
+elif rpm -q -f /bin/ls >/dev/null 2>&1
+then
+  case "$(arch)" in
+    x86_64)
+     xdg-mime default cryptoarm-gost.desktop x-scheme-handler/cryptoarm ;;
+    i368)
+      xdg-mime default cryptoarm-gost.desktop x-scheme-handler/cryptoarm;;
+    *)
+      exit 1
+      ;;
+  esac
+else
+  exit 1
+fi
