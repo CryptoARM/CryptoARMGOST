f620ac0c6e5035211fc854c475d0e8eb8068ed05
Алексей Поликарпов <mecrewcut@gmail.com>
2020-09-11 14:56:51 +0300

    #599- https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/599
    fixed license diagnostic (if key in license.lic was spoiled)

--- app/components/Diagnostic/Diagnostic.tsx
+++ app/components/Diagnostic/Diagnostic.tsx
@@ -156,7 +156,7 @@ class Diagnostic extends React.Component<any, IDiagnosticState> {
   componentWillReceiveProps(nextProps: any) {
     const { certificatesLoaded, loadingLicense } = this.props;
     const { errors } = this.state;
-
+  
     if (nextProps.statusLicense === false && nextProps.lic_format === "NONE" && nextProps.verifiedLicense == true && loadingLicense === false) {
       this.setState({
         errors: [...this.state.errors, {
@@ -166,7 +166,15 @@ class Diagnostic extends React.Component<any, IDiagnosticState> {
       });
 
     }
-
+   
+    if (nextProps.dataLicense && nextProps.lic_format === undefined && nextProps.statusLicense === false && nextProps.verifiedLicense == true && loadingLicense === false) {     
+      this.setState({
+        errors: [...this.state.errors, {
+          important: WARNING,
+          type: NO_CORRECT_CRYPTOARM_LICENSE,
+        }],
+      });
+    }
     if (nextProps.lic_format === "dlv" && nextProps.statusLicense === false && nextProps.verifiedLicense == true && loadingLicense === false) {
 
       this.setState({
@@ -293,7 +301,7 @@ class Diagnostic extends React.Component<any, IDiagnosticState> {
           <div className="row nobottom">
             <div className="diagnostic-content-item">
               <div className="col s6 m5 l6 problem-contaner">
-
+                
                 <Problems errors={cspErrors.length ? cspErrors : errors} activeError={this.state.activeError} onClick={this.handleClickOnError} />
               </div>
               <div className="col s6 m7 l6 problem-contaner">
